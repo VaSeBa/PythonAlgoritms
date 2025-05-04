ef2df867f649f14cfee8f3c9e2f4d05337a834c4
vaseba <badasin.valentin@yandex.ru>
2025-05-04 15:19:03 +0500

    init commit

---
+++ binary_search.py
@@ -0,0 +1,28 @@
+import random
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    step = 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        print(f"step = {step}")
+        step += 1
+        if guess == item:  # число найдено
+            return mid
+        if guess > item:  # число больше
+            high = mid - 1
+        else:  # число меньше
+            low = mid + 1
+    return None
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers2 = [x for x in range(1, 1001)]
+target = 670
+
+result = binary_search(numbers2, target)
+print(f"Индекс элемента {target}: {result}, длинна списка = {len(numbers2)}, индекс первого элемента = {numbers.index(1)}")
